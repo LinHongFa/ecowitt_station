8380dc51fb188abaaeee734a5573ee46bd6540e8
LinHongFa <956110217@qq.com>
2022-04-06 15:03:30 +0800

    Home Assistant with ecowitt

---
+++ const.py
@@ -0,0 +1,12 @@
+"""Define constants for the Ecowitt component."""
+import logging
+
+DOMAIN = "ecowitt_station"
+LOGGER = logging.getLogger(__package__)
+
+ATTR_LAST_DATA = "last_data"
+
+CONF_APP_KEY = "app_key"
+
+TYPE_SOLARRADIATION = "solarradiation"
+TYPE_SOLARRADIATION_LX = "solarradiation_lx"
